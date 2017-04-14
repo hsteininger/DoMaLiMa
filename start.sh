17fd57422730867ae3cb83a92815bd8b1401d4c9
Herbert Steininger <herbert_steininger@psych.mpg.de>
2017-04-14 21:53:18 +0200

    start.sh rewritten

--- start.sh
+++ start.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-/usr/local/MatLab/2017a/etc/lmstart -v -u matlab
-/usr/local/MatLab/2017a/etc/lmstat
-
-/bin/bash
+while [ true ]
+do
+	MATLAB=$(ps aux | grep lm_TMW)
+		if [ ! "$MATLAB" ]
+		then
+			/usr/local/MatLab/2017a/etc/lmstart -v -u matlab
+		fi
+sleep 15
+MATLAB=""
+done
